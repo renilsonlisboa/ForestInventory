9ffc16260cd7216253ec7279878ce266be6b1991
renilsonlisboa <renilsonlisboajunior@gmail.com>
2024-11-03 18:27:50 -0300

    modified:   Project.toml modified:   src/TCCFunctions.jl

--- src/TCCFunctions.jl
+++ src/TCCFunctions.jl
@@ -1,5 +1,12 @@
 module TCCFunctions
 
 # Write your package code here.
+using GenieFramework, DataFrames, CSV, Random, PlotlyBase
+
+export RunApp
+
+    function RunApp()
+        GenieFramework.loadapp()
+    end
 
 end
