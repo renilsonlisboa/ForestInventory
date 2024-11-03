dabafb8686d5e71603ea7026467aca99c3c3a3f5
renilsonlisboa <renilsonlisboajunior@gmail.com>
2024-11-03 19:43:18 -0300

    modified:   log/dev-2024-11-03.log 	modified:   src/TCCFunctions.jl

--- src/TCCFunctions.jl
+++ src/TCCFunctions.jl
@@ -2,13 +2,13 @@ module TCCFunctions
 
 # Write your package code here.
 using Genie, GenieFramework, DataFrames, CSV, Random, PlotlyBase
-include(joinpath(@__DIR__, "src/app.jl")) # Amostragem Aleatória Simples
+include(joinpath(@__DIR__, "app.jl")) # Amostragem Aleatória Simples
 @genietools
 
 export RunApp
 
     function RunApp()
-        Genie.loadapp("src/.")
+        Genie.loadapp("")
         up()
     end
 
